76771325e01451e9104d42907cf8590d7c291e3c
sahan <101819553+omerfshan@users.noreply.github.com>
2025-06-01 12:31:19 +0300

    değişiklikler

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,44 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-    AudioManager manager;
+    private AudioManager manager;
+
     public GameObject OptionsPanel;
     public GameObject MenuPanel;
     public SelectedUI selectedUI;
-    public GameObject SelectButton;
+    public GameObject PauseButton;
+    public GameObject PlayButton;
+
+    [Header("Options Panelindeki İlk Seçilecek UI Objesi")]
+    public GameObject firstOptionButton;
+
     void Start()
     {
-      manager=FindAnyObjectByType<AudioManager>();  
+        manager = FindAnyObjectByType<AudioManager>();
+
+        if (firstOptionButton == null)
+        {
+            Debug.LogWarning("firstOptionButton atanmadı! Gamepad kontrolü olmayabilir.");
+        }
     }
+
     public void PlayGame()
     {
         manager.ButtonSelectedSound();
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
     public void QuitGame()
     {
         manager.ButtonSelectedSound();
-        Debug.Log("QU�T!");
+        Debug.Log("QUIT!");
         Application.Quit();
     }
+
     public void OpenOptions()
     {
         manager.ButtonSelectedSound();
         OptionsPanel.SetActive(true);
         MenuPanel.SetActive(false);
-        selectedUI.SelectedButton = SelectButton;
+       
 
-
+       
     }
-     public void CloseOptions()
+
+    public void CloseOptions()
     {
         manager.ButtonSelectedSound();
         OptionsPanel.SetActive(false);
-        MenuPanel.SetActive(true);  
-    }
+        MenuPanel.SetActive(true);
 
+        
+    }
 }
